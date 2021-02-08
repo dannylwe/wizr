757813229ad0ea4661d6d04edfd28770a2f185d3
unknown <dannylwe11@gmail.com>
2021-02-08 21:30:43 +0300

    starter app

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-// import request from 'requests';
-let helloWorld = 'Hello World!';
-console.log(`${helloWorld} this is some ES6 JavaScript code`);
+import express from 'express'
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
