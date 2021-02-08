a501498528a2ddc8e7326483e06ce1268753984b
unknown <dannylwe11@gmail.com>
2021-02-08 22:16:56 +0300

    simple upload to server using template

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import multer from 'multer';
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -13,10 +14,28 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan('combined'));
 
+app.use(express.static('public'));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        const { originalname } = file;
+        cb(null, originalname);
+    }
+})
+const upload = multer({ storage });
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
+app.post('/upload', upload.array('file'), (req, res) => {
+    return res.json({ status: 'OK', uploaded: req.files.length });
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
-})
+});
