fa691c1e6f5c7022aba65f68a9176e68dac32f0f
unknown <dannylwe11@gmail.com>
2021-02-08 21:35:01 +0300

    middlewares

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
 const app = express()
 const port = process.env.PORT || 3000
+
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors());
+app.use(helmet());
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server is running at http://localhost:${port}`)
 })
