d09b43e750e90a82e8da8a6fe61beb20bd6c23ac
Daniel Lwetabe <dannylwe11@gmail.com>
2021-02-22 01:07:12 +0300

    added slides presentation

---
+++ ui/src/components/SliderData.js
@@ -0,0 +1,11 @@
+export const SliderData = [
+    {
+        image: 'https://res.cloudinary.com/dk6gfgrmc/image/upload/v1613937783/juhl8x1oup7awtnts8u6.png'
+    },
+    {
+        image: 'https://res.cloudinary.com/dk6gfgrmc/image/upload/v1613937798/pum6wabd3eagme2uh69a.png'
+    },
+    {
+        image: 'https://res.cloudinary.com/dk6gfgrmc/image/upload/v1613937808/yzjybl9795ms87soxgcs.png'
+    }
+];
